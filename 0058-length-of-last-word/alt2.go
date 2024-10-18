8ebf8edff77f2137dca93165488eb1f9b36dc178
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-10-18 13:19:48 +0700

    Time: 0 ms (100%), Space: 4.1 MB (6.96%) - LeetHub

---
+++ 0058-length-of-last-word/alt2.go
@@ -0,0 +1,17 @@
+// alternative idea #2
+
+func lengthOfLastWord(s string) int {
+    // constraint: 1 <= s.length <= 10^4
+    if 1 <= len(s) && len(s) <= 10000 {
+        // splitting s string by " "
+        ss := strings.Split(s, " ")
+
+        // return the last word length
+        for i := len(ss) - 1; i >= 0; i-- {
+            if ss[i] != " " && ss[i] != "" {
+                return len(ss[i])
+            }
+        }
+    }
+    return -1
+}
