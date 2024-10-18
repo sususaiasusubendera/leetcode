c5f4ab8f32b150e6f2e2fdf42ae7fc9a674f5b75
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-10-18 13:10:21 +0700

    Time: 0 ms (100%), Space: 4.1 MB (6.96%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.go
+++ 0058-length-of-last-word/0058-length-of-last-word.go
@@ -1,21 +1,15 @@
 func lengthOfLastWord(s string) int {
     // constraint: 1 <= s.length <= 10^4
     if 1 <= len(s) && len(s) <= 10000 {
-        i := len(s) - 1
-        lastWordLength := 0
+        // splitting s string by " "
+        ss := strings.Split(s, " ")
 
-        // ignore spaces at the end of the string
-        for i >= 0 && s[i] == ' ' {
-            i--
+        // return the last word length
+        for i := len(ss) - 1; i >= 0; i-- {
+            if ss[i] != " " && ss[i] != "" {
+                return len(ss[i])
+            }
         }
-
-        // count the characters of the last word of the string
-        for i >= 0 && s[i] != ' ' {
-            lastWordLength++
-            i--
-        }
-
-        return lastWordLength
     }
     return -1
 }
