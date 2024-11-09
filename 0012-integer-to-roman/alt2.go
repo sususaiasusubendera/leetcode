683dd07a430eca83e50d92c244cc56d888bfcf51
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-11-09 14:40:32 +0700

    Time: 0 ms (100%), Space: 5.05 MB (22.15%) - LeetHub

---
+++ 0012-integer-to-roman/alt2.go
@@ -0,0 +1,34 @@
+func intToRoman(num int) string {
+    type NumRom struct {
+        Num int
+        Rom string
+    }
+
+    conv := []NumRom{
+        {1, "I"},
+        {4, "IV"},
+        {5, "V"},
+        {9, "IX"},
+        {10, "X"},
+        {40, "XL"},
+        {50, "L"},
+        {90, "XC"},
+        {100, "C"},
+        {400, "CD"},
+        {500, "D"},
+        {900, "CM"},
+        {1000, "M"},
+    }
+
+    roman := ""
+    n := len(conv)-1
+    for num > 0 && n >= 0 {
+        if num >= conv[n].Num {
+            num -= conv[n].Num
+            roman += conv[n].Rom
+        } else {
+            n--
+        }
+    }
+    return roman
+}
