626e49bd83b34d78697c3efca0c9ebca2a496181
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-11-14 21:37:40 +0700

    Time: 0 ms (100%), Space: 3.68 MB (47.53%) - LeetHub

---
+++ 0202-happy-number/alt2.go
@@ -0,0 +1,20 @@
+// More effective solution using Floyd's Cycle Detection (Tortoise and Hare) approach
+// The other solution is too 'brute'
+func isHappy(n int) bool {
+    slow, fast := n, next(n)
+    for fast != 1 && slow != fast {
+        slow = next(slow)
+        fast = next(next(fast))
+    }
+    return fast == 1
+}
+
+func next(n int) int {
+    sum := 0
+    for n > 0 {
+        digit := n % 10
+        sum += digit * digit
+        n /= 10
+    }
+    return sum
+}
