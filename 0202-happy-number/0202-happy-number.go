df44c85d9b8f3cfa932cfa7994b488cb52c57438
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-11-14 21:37:10 +0700

    Time: 0 ms (100%), Space: 3.7 MB (47.53%) - LeetHub

--- 0202-happy-number/0202-happy-number.go
+++ 0202-happy-number/0202-happy-number.go
@@ -1,31 +1,18 @@
 func isHappy(n int) bool {
-    count := 0
-
-    happy := false
-    for !happy {
-        temp := 0
-        for n > 0 {
-            temp += pow(n % 10, 2)
-            n /= 10
-        }
-        count++
-        if temp == 1 {
-            return !happy
-        } else {
-            n = temp
-        }
-        if count > 100 {
-            return happy
-        }
+    slow, fast := n, next(n)
+    for fast != 1 && slow != fast {
+        slow = next(slow)
+        fast = next(next(fast))
     }
-
-    return happy
+    return fast == 1
 }
 
-func pow(n, p int) int {
-    a := 1
-    for i := 0; i < p; i++ {
-        a *= n
+func next(n int) int {
+    sum := 0
+    for n > 0 {
+        digit := n % 10
+        sum += digit * digit
+        n /= 10
     }
-    return a
+    return sum
 }
