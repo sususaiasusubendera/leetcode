ef45e83a8a8edbf60f42029a3ba417654b97ab4b
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-11-28 10:09:18 +0700

    Time: 0 ms (100%), Space: 8 MB (31.47%) - LeetHub

---
+++ 0135-candy/0135-candy.go
@@ -0,0 +1,26 @@
+func candy(ratings []int) int {
+    candies := make([]int, len(ratings))
+    for i := 0; i < len(candies); i++ {
+        candies[i] = 1
+    }
+
+    for i := 1; i < len(ratings); i++ {
+        if ratings[i] > ratings[i-1] {
+            candies[i] = candies[i-1] + 1
+        }
+    }
+
+    for i := len(ratings)-2; i >= 0; i-- {
+        if ratings[i] > ratings[i+1] {
+            if candies[i] <= candies[i+1] {
+                candies[i] = candies[i+1] + 1
+            }
+        }
+    }
+
+    sum := 0
+    for i := 0; i < len(candies); i++ {
+        sum += candies[i]
+    }   
+    return sum
+}
