59f1f7214cc577d5e50fa157e0f709651926fb33
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-12-03 19:30:27 +0700

    Time: 0 ms (100%), Space: 8.8 MB (69.31%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.go
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.go
@@ -7,10 +7,36 @@
  * }
  */
 func countNodes(root *TreeNode) int {
+    l := leftHeight(root)
+    r := rightHeight(root)
+    if l == r {
+        return pow(2, l) - 1
+    } else {
+        return 1 + countNodes(root.Left) + countNodes(root.Right)
+    }
+}
+
+func leftHeight(root *TreeNode) int {
     if root == nil {
         return 0
     }
-
-    return 1 + countNodes(root.Left) + countNodes(root.Right)
+    return 1 + leftHeight(root.Left)
 }
-// time complexity: O(n)
+
+func rightHeight(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    return 1 + rightHeight(root.Right)
+}
+
+func pow(n, p int) int {
+    x := 1
+    for i := 0; i < p; i++ {
+        x *= n
+    }
+    return x
+}
+
+// binary search - DFS
+// time complexity: O(log^2(n))
