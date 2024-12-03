c70c5f209196b2546f675b7c8e9a93b32682f5f8
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-12-03 10:31:27 +0700

    Time: 0 ms (100%), Space: 9.6 MB (10.53%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/alt2.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    return 1 + countNodes(root.Left) + countNodes(root.Right)
+}
+// DFS - brute force
+// time complexity: O(n)
