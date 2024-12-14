a51401439fea14d1943d5d8c223698deb2047d87
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-12-14 23:57:12 +0700

    Time: 661 ms (66.25%), Space: 0B (100%) - LeetHub

---
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+WITH 
+    FirstOrder AS (
+        SELECT
+            customer_id,
+            MIN(order_date) AS first_order_date
+        FROM Delivery
+        GROUP BY customer_id
+    ),
+    FirstOrderDetail AS (
+        SELECT
+            d.customer_id,
+            d.order_date,
+            d.customer_pref_delivery_date,
+            CASE WHEN
+                d.order_date = d.customer_pref_delivery_date
+                    THEN 1
+                    ELSE 0
+                END AS is_immediate
+        FROM Delivery d
+        INNER JOIN FirstOrder fo
+        ON d.customer_id = fo.customer_id AND d.order_date = fo.first_order_date
+    )
+SELECT ROUND(AVG(is_immediate) * 100, 2) AS immediate_percentage
+FROM FirstOrderDetail
