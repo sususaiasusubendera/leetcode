ff26db11c817c362f905fa89673bd26f186a32c6
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-12-19 12:08:10 +0700

    Time: 420 ms (97.79%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,24 +1,25 @@
 # Write your MySQL query statement below
-# union before and after 2019-08-16
-SELECT DISTINCT
-    product_id,
-    10 AS price
-FROM Products
-WHERE product_id NOT IN (
-    SELECT DISTINCT product_id
-    FROM Products
-    WHERE change_date <= "2019-08-16"
-)
-UNION ALL
-SELECT
-    product_id,
-    new_price AS price
-FROM Products
-WHERE (product_id, change_date) IN (
+# window function
+WITH Latest AS (
     SELECT
         product_id,
-        MAX(change_date) AS latest
+        new_price,
+        change_date
     FROM Products
     WHERE change_date <= "2019-08-16"
-    GROUP BY product_id
+),
+RankedPrice AS (
+    SELECT
+        product_id,
+        new_price,
+        change_date,
+        ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS row_num
+    FROM Latest
 )
+
+SELECT
+    p.product_id,
+    IFNULL(rp.new_price, 10) AS price
+FROM (SELECT DISTINCT product_id FROM Products) p
+LEFT JOIN RankedPrice rp
+ON p.product_id = rp.product_id AND rp.row_num = 1
