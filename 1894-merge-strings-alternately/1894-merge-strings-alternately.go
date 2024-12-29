c28cc24bb47303fed822302822a3505f16b4262b
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-12-29 22:49:28 +0700

    Time: 0 ms (100%), Space: 3.9 MB (76.82%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.go
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.go
@@ -1,28 +1,16 @@
 func mergeAlternately(word1 string, word2 string) string {
-    result := ""
-    turn := 1
-    one, two := 0, 0
-    steps1, steps2 := len(word1), len(word2)
-    for steps1 > 0 || steps2 > 0  {
-        if turn == 1 {
-            result += string(word1[one])
-            if one < len(word1) - 1 {
-                one++
-            }
-            steps1--
-            if steps2 > 0 {
-                turn = 2
-            }
-        } else {
-            result += string(word2[two])
-            if two < len(word2) - 1 {
-                two++
-            }
-            steps2--
-            if steps1 > 0 {
-                turn = 1
-            }
+    var builder strings.Builder
+    p1, p2 := 0, 0 // pointer 1, pointer 2
+    l1, l2 := len(word1), len(word2) // length 1, length 2
+    for p1 < l1 || p2 < l2 {
+        if p1 < l1 {
+            builder.WriteByte(word1[p1])
+            p1++
+        }
+        if p2 < l2 {
+            builder.WriteByte(word2[p2])
+            p2++
         }
     }
-    return result
+    return builder.String()
 }
