8ea41daeef3a8ade350a5cd2a3c00514fdfe5b34
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2024-12-30 12:14:25 +0700

    Time: 0 ms (100%), Space: 8 MB (72.22%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.go
+++ 0605-can-place-flowers/0605-can-place-flowers.go
@@ -1,36 +1,12 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
-    if n == 0 {
-        return true
-    }
-
-    if len(flowerbed) == 1 && flowerbed[0] == 0 {
-        return true
-    }
-
-    for i := 0; i < len(flowerbed); i++ {
-        if flowerbed[i] != 1 {
-            if i == 0 {
-                if flowerbed[i+1] == 0 {
-                    n--
-                    flowerbed[i] = 1
-                }
-            } else if i == len(flowerbed) - 1 {
-                if flowerbed[i-1] == 0 {
-                    n--
-                    flowerbed[i] = 1
-                }
-            } else {
-                if flowerbed[i-1] == 0 && flowerbed[i + 1] == 0 {
-                    n--
-                    flowerbed[i] = 1
-                }
-            }
+    for i := 0; i < len(flowerbed) && n > 0; i++ {
+        if flowerbed[i] == 0 && 
+            (i == 0 || flowerbed[i-1] == 0) && 
+            (i == len(flowerbed) - 1 || flowerbed[i+1] == 0) {
+            flowerbed[i] = 1
+            n--
         }
     }
 
-    if n <= 0 {
-        return true
-    } else {
-        return false
-    }
+    return n == 0
 }
