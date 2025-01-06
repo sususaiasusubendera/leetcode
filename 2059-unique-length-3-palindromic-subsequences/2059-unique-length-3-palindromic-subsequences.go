cff120546ec50cc1242e3d1dc730ec524bc4613c
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-01-06 20:15:06 +0700

    Time: 279 ms (57.41%), Space: 7.9 MB (74.44%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.go
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.go
@@ -1,9 +1,11 @@
 func countPalindromicSubsequence(s string) int {
+    // initialize an array with [-1, -1] (for position tracking [start, end])
     order := make([][2]int, 26)   
     for i := 0; i < len(order); i++ {
         order[i] = [2]int{-1, -1}
     }
 
+    // traverse the s, locate 'start index' and 'end index' of s[i] if any
     for i, c := range s {
         idx := c - 'a'
         if order[idx][0] != -1 {
@@ -13,11 +15,15 @@ func countPalindromicSubsequence(s string) int {
         }
     }
 
+    // count unique middle characters between start and end
     count := 0
     for _, pos := range order {
-        if pos[1] - pos[0] > 1 {
-            unique := make(map[rune]struct{})
+        // check if any middle characters
+        if pos[1] - pos[0] > 1 { 
+            // hash set using struct{} is better (space) than bool, CMIIW
+            unique := make(map[rune]struct{}) 
             for _, mid := range s[pos[0]+1 : pos[1]] {
+                // mark the unique character using struct{}{}
                 unique[mid] = struct{}{}
             }
             count += len(unique)
@@ -26,5 +32,3 @@ func countPalindromicSubsequence(s string) int {
 
     return count
 }
-
-// temp
