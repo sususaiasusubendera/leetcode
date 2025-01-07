c98d066cb1a35c673738f0030505186891a85f9a
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-01-07 20:20:38 +0700

    Time: 0 ms (100%), Space: 13 MB (97.81%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.go
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.go
@@ -1,8 +1,11 @@
 func shiftingLetters(s string, shifts [][]int) string {
+    // initiate diff slice with size len(s)+1 (for difference array approach)
     diff := make([]int, len(s)+1)
 
+    // the difference array approach
     for _, shift := range shifts {
         start, end, direction := shift[0], shift[1], shift[2]
+        // mark the start index and end+1 index of the diff according to the direction of shift
         if direction == 1 {
             diff[start]++
             diff[end+1]--
@@ -11,13 +14,13 @@ func shiftingLetters(s string, shifts [][]int) string {
             diff[end+1]++
         }
     }
-
+    
     currentShift := 0
     result := make([]byte, len(s))
     for i := 0; i < len(s); i++ {
         currentShift += diff[i]
-        shifted := (int(s[i]-'a') + currentShift) % 26
-        if shifted < 0 {
+        shifted := (int(s[i]-'a') + currentShift) % 26 // shift the s[i] according to the currentShift
+        if shifted < 0 { // case for backward shift past 'a'
             shifted += 26
         }
         result[i] = byte(shifted) + 'a'
@@ -26,4 +29,4 @@ func shiftingLetters(s string, shifts [][]int) string {
     return string(result)
 }
 
-// temp
+// differecne array approach
