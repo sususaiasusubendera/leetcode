b5750238763576c8ce9d1a119bbafa9988f8b21c
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-01-08 11:47:25 +0700

    Time: 0 ms (100%), Space: 4.3 MB (63.64%) - LeetHub

---
+++ 1524-string-matching-in-an-array/alt2.go
@@ -0,0 +1,20 @@
+func stringMatching(words []string) []string {
+    seen := map[string]bool{}
+    result := []string{}
+    for i := 0; i < len(words); i++ {
+        for j := 0; j < len(words); j++ {
+            if i != j && strings.Contains(words[j], words[i]) {
+                if !seen[words[i]] {
+                    result = append(result, words[i])
+                    seen[words[i]] = true
+                }
+                break
+            }   
+        }
+    }
+
+    return result
+}
+
+// brute force
+// time: O(n^2); space: O(n)
