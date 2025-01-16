70be8331e4b52b5fa1d1c739083c9c8dbb4dd2b6
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-01-16 16:41:50 +0700

    Time: 0 ms (100%), Space: 3.9 MB (100%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.go
+++ 2509-minimize-xor/2509-minimize-xor.go
@@ -1,54 +1,27 @@
 func minimizeXor(num1 int, num2 int) int {
-    // count set bits ('1' in bin) of num2
-    setBits2 := bits.OnesCount(uint(num2)) // setBits2: set bits of num2
-    // convert int num1 into binary string
-    binStr1 := strconv.FormatInt(int64(num1), 2) // binStr1: binary string of num1
-    
-    // copy binStr1 into slice of byte x
-    binSoBX := []byte(binStr1) // binSoBX: binary slice of byte x
-
-    // count set bits of x
-    setBitsX := 0 // setBitsX: set bits of x
-    for i := 0; i < len(binSoBX); i++ {
-        if binSoBX[i] == '1' {
-            setBitsX++
+    setBits2 := bits.OnesCount(uint(num2)) // count set bits of num 2
+    x := 0
+    // use bits from num1, iterate from msb to minimize xor
+    for i := 31; i >= 0 && setBits2 > 0; i-- {
+        if (num1 & (1 << i)) != 0 { // check if i-th index bit in num1 is '1' (or not '0')
+            x |= (1 << i) // set the bit of x in i-th index (with '1')
+            setBits2--
         }
     }
-    
-    // add extra bits '1' if needed (setBitsX < setBits2)
-    n := len(binSoBX)-1
-    for setBitsX < setBits2 && n >= 0 {
-        if binSoBX[n] == '0' {
-            binSoBX[n] = '1'
-            setBitsX++
+
+    // if more set bits are needed, add from lsb
+    for i := 0; setBits2 > 0; i++ {
+        if x & (1 << i) == 0 { // check if i-th index bit in x is '0'
+            x |= (1 << i)
+            setBits2--
         }
-        n--
     }
 
-    // If still not enough setBits, prepend '1' to the binary string
-    for setBitsX < setBits2 {
-        binSoBX = append([]byte{'1'}, binSoBX...) // Add '1' to the front
-        setBitsX++
-    }
-
-    // remove excess bits '1' if needed (setBitsX > setBits2)
-    n = len(binSoBX)-1
-    for setBitsX > setBits2 && n >= 0 {
-        if binSoBX[n] == '1' {
-            binSoBX[n] = '0'
-            setBitsX--
-        }
-        n--
-    }
-
-    // convert the binary string x back to an integer
-    x, _ := strconv.ParseInt(string(binSoBX), 2, 64)
-    return int(x)
+    return x
 }
 
-// naive solution
+// bitwise operation approach
+// time: O(1)
+// space: O(1)
 
-// p.s. xor
-// a xor a = 0, e.g. 11 xor 11 = 0 (3 xor 3 = 0)
-// the more digits that are the same, the lower the xor value
-// for minimal change of xor value, add/remove '1' from lsb to msb
+
