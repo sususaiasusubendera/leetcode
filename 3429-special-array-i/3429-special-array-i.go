028591ed65a117d185de242deb55cd39b22eb0d5
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-02-01 14:19:07 +0700

    Time: 0 ms (100%), Space: 4.6 MB (100%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.go
+++ 3429-special-array-i/3429-special-array-i.go
@@ -1,27 +1,9 @@
 func isArraySpecial(nums []int) bool {
-    if len(nums) == 1 {
-        return true
-    }
-
-    firstEven := nums[0] % 2 == 0
-    if firstEven { // if the first element is even
-        for i := 1; i < len(nums); i++ {
-            if i % 2 == 1 && nums[i] % 2 == 0 {
-                return false
-            } else if i % 2 == 0 && nums[i] % 2 == 1 {
-                return false
-            }
-        }
-    } else { // if the first element is odd
-        for i := 1; i < len(nums); i++ {
-            if i % 2 == 1 && nums[i] % 2 == 1 {
-                return false
-            } else if i % 2 == 0 && nums[i] % 2 == 0 {
-                return false
-            }
+    for i := 1; i < len(nums); i++ {
+        if (nums[i] % 2) == (nums[i-1] % 2) {
+            return false
         }
     }
-
     return true
 }
 
