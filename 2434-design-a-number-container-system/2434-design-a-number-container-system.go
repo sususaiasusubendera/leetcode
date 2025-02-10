14b01990599fc8b1d4fc537ff85699e2f0119152
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-02-10 23:59:41 +0700

    Time: 159 ms (40%), Space: 58.5 MB (14.43%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.go
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.go
@@ -1,58 +1,140 @@
+// MAIN
+type NumberContainers struct {
+    mapIdxNum map[int]int // index -> number
+    mapNumIdxs map[int]*MinHeap // number -> indexes (priority queue)
+    validIdx map[int]map[int]bool // number -> (indexes -> bool (set of indexes))
+}
+
+
+func Constructor() NumberContainers {
+    return NumberContainers {
+        mapIdxNum: map[int]int{},
+        mapNumIdxs: map[int]*MinHeap{},
+        validIdx: map[int]map[int]bool{},
+    }
+}
+
+
+func (this *NumberContainers) Change(index int, number int) {
+    // delete the index from validIdx[oldNum] (the index becomes invalid) if it already exists
+    if oldNum, exists := this.mapIdxNum[index]; exists {
+        delete(this.validIdx[oldNum], index)
+    }
+
+    // update index with the latest number
+    this.mapIdxNum[index] = number
+
+    // initialize a heap (priority queue) if it's a new number
+    if _, exists := this.mapNumIdxs[number]; !exists {
+        this.mapNumIdxs[number] = &MinHeap{}
+        heap.Init(this.mapNumIdxs[number])
+        this.validIdx[number] = map[int]bool{}
+    }
+
+    // push the index into the priority queue and mark it valid (in validIdx[number])
+    heap.Push(this.mapNumIdxs[number], index)
+    this.validIdx[number][index] = true
+}
+
+
+func (this *NumberContainers) Find(number int) int {
+    // return -1 if the number doesn't exist
+    if _, exists := this.mapNumIdxs[number]; !exists {
+        return -1
+    }
+
+    // get the smallest index that is still valid 
+    for this.mapNumIdxs[number].Len() > 0 {
+        top := (*this.mapNumIdxs[number])[0]
+        if this.validIdx[number][top] {
+            return top
+        }
+        heap.Pop(this.mapNumIdxs[number])
+    }
+
+    return -1
+}
+
+// map and priority queue approach
+// time: O(log k + m log k)
+// space: O(n + mk)
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Change(index,number);
+ * param_2 := obj.Find(number);
+**/
+
+// MINHEAP
 type MinHeap []int
 
-func (h MinHeap) Len() int { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] } // Ascending order
-func (h MinHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *MinHeap) Len() int {
+    return len(*h)
+}
+
+func (h *MinHeap) Less(i, j int) bool {
+    return (*h)[i] < (*h)[j]
+}
+
+func (h *MinHeap) Swap(i, j int) {
+    (*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *MinHeap) Push(x interface{}) {
+    *h = append(*h, x.(int))
+}
+
 func (h *MinHeap) Pop() interface{} {
     old := *h
     n := len(old)
     x := old[n-1]
-    *h = old[0 : n-1]
+    *h = old[:n-1]
     return x
 }
 
-// Main structure
+// -------
 
-type NumberContainers struct {
-    mapIdxNum  map[int]int   // {index: number}
-    mapNumIdxs map[int]*MinHeap // {number: heap of indices}
-}
+// >>> authentic TLE map solution <<<
 
-func Constructor() NumberContainers {
-    return NumberContainers{
-        mapIdxNum:  make(map[int]int),
-        mapNumIdxs: make(map[int]*MinHeap),
-    }
-}
+// type NumberContainers struct {
+//     mapIdxNum map[int]int
+//     mapNumIdxs map[int][]int
+// }
 
-func (this *NumberContainers) Change(index int, number int) {
-    // If the index already has an existing number, remove it from the previous heap
-    if oldNum, exists := this.mapIdxNum[index]; exists && oldNum != number {
-        // No need to remove it directly from the heap, just mark that this index no longer belongs to `oldNum`
-        heap.Init(this.mapNumIdxs[oldNum]) // Rebuild heap to clean up invalid indices
-    }
 
-    // Update the number at the index
-    this.mapIdxNum[index] = number
+// func Constructor() NumberContainers {
+//     return NumberContainers{
+//         map[int]int{},
+//         map[int][]int{},
+//     }
+// }
 
-    // Insert index into the heap for the new number
-    if this.mapNumIdxs[number] == nil {
-        this.mapNumIdxs[number] = &MinHeap{}
-    }
-    heap.Push(this.mapNumIdxs[number], index)
-}
 
-func (this *NumberContainers) Find(number int) int {
-    if h, exists := this.mapNumIdxs[number]; exists {
-        // Ensure the heap is still valid with the correct indices
-        for h.Len() > 0 {
-            smallestIndex := (*h)[0] // Check the smallest index
-            if this.mapIdxNum[smallestIndex] == number {
-                return smallestIndex
-            }
-            heap.Pop(h) // Remove outdated index
-        }
-    }
-    return -1
-}
+// func (this *NumberContainers) Change(index int, number int)  {
+//     if oldNum, exists := this.mapIdxNum[index]; exists {
+//         idxs := this.mapNumIdxs[oldNum]
+//         for i, idx := range idxs {
+//             if idx == index {
+//                 this.mapNumIdxs[oldNum] = append(idxs[:i], idxs[i+1:]...)
+//             }
+//             break
+//         }
+
+//         if len(this.mapNumIdxs[oldNum]) == 0 {
+//             delete(this.mapNumIdxs, oldNum)
+//         }
+//     }
+
+//     this.mapIdxNum[index] = number
+//     this.mapNumIdxs[number] = append(this.mapNumIdxs[number], index)
+//     sort.Ints(this.mapNumIdxs[number])
+// }
+
+
+// func (this *NumberContainers) Find(number int) int {
+//     if len(this.mapNumIdxs[number]) == 0 {
+//         return -1
+//     }
+//     return this.mapNumIdxs[number][0]
+// }
