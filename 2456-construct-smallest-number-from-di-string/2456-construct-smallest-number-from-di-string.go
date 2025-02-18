65b0bb27f5889c674bd56801575feb8ab4fcec40
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-02-18 22:59:01 +0700

    Time: 0 ms (100%), Space: 3.8 MB (90%) - LeetHub

---
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.go
@@ -0,0 +1,16 @@
+func smallestNumber(pattern string) string {
+	stack := make([]byte, 0, len(pattern)+1)
+	res := make([]byte, 0, len(pattern)+1)
+	for i := 0; i <= len(pattern); i++ {
+		stack = append(stack, byte('1'+i))
+		if i == len(pattern) || pattern[i] == 'I' {
+			for j := len(stack)-1; j >= 0; j-- {
+				res = append(res, stack[j])
+			}
+			stack = stack[:0]
+		}
+	}
+	return string(res)
+}
+
+// notice me senpai
