dd6e899ec192d1942411c85396425a20cbe53856
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-03-01 23:29:54 +0700

    Time: 0 ms (100%), Space: 4.7 MB (71.7%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.go
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.go
@@ -1,24 +1,22 @@
 func applyOperations(nums []int) []int {
-    result := make([]int, len(nums))
-    p := 0
-    for i:= 0; i < len(nums)-1; i++ {
-        if nums[i] == nums[i+1] {
+    writeIndex := 0
+    for i:= 0; i < len(nums); i++ {
+        if i < len(nums)-1 && nums[i] == nums[i+1] && nums[i] != 0 {
             nums[i] *= 2
             nums[i+1] = 0
         }
 
         if nums[i] != 0 {
-            result[p] = nums[i]
-            p++
+            if i != writeIndex {
+                nums[i], nums[writeIndex] = nums[writeIndex], nums[i]
+            }
+            writeIndex++
         }
     }
 
-    if nums[len(nums)-1] != 0 {
-        result[p] = nums[len(nums)-1]
-    }
-
-    return result
+    return nums
 }
 
+// one pass
 // time: O(n)
-// space: O(n)
+// space: O(1)
