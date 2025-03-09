8a68ea2b33649254e6bb4693b31775020f4201f3
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-03-09 18:02:14 +0700

    Time: 98 ms (40%), Space: 13.2 MB (20%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.go
@@ -0,0 +1,26 @@
+func numberOfAlternatingGroups(colors []int, k int) int {
+	extendedColors := append(colors, colors[:k-1]...)
+
+	n := len(extendedColors)
+	countAltGroups := 0
+	left, right := 0, 1
+
+	for right < n {
+		if extendedColors[right] == extendedColors[right-1] {
+			left = right
+			right++
+			continue
+		}
+
+		right++
+
+		if right-left < k {
+			continue
+		}
+
+		countAltGroups++
+		left++
+	}
+
+    return countAltGroups
+}
