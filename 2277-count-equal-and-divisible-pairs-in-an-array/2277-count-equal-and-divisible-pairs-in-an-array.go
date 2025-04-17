1b5018364ac2dc07f04acb15e0fa59d4a0ac5f6f
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-04-17 23:31:00 +0700

    Time: 0 ms (100%), Space: 4.5 MB (69.39%) - LeetHub

---
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.go
@@ -0,0 +1,16 @@
+func countPairs(nums []int, k int) int {
+    count := 0
+    for i := 0; i < len(nums)-1; i++ {
+        for j := i+1; j < len(nums); j++ {
+            if nums[i] == nums[j] && (i*j) % k == 0 {
+                count++
+            }
+        }
+    }
+
+    return count
+}
+
+// brute force
+// time: O(n^2)
+// space: O(1)
