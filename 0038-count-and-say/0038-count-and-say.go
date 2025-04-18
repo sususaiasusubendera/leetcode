78d9dfc458fe185265d5e8817a0295ce2081329d
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-04-18 23:53:09 +0700

    Time: 8 ms (34.81%), Space: 9.7 MB (23.64%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.go
@@ -0,0 +1,41 @@
+func countAndSay(n int) string {
+    if n == 1 {
+        return "1"
+    }
+
+    result := "1"
+    for i := 2; i <= n; i++ {
+        result = strRLE(parseRLE(result))
+    }
+
+    return result
+}
+
+// two pointer
+
+func parseRLE(str string) [][2]int {
+    slc := [][2]int{}
+    p1 := 0
+    for p1 < len(str) {
+        countSameNum := 0
+        p2 := p1
+        for p2 < len(str) && str[p2] == str[p1] {
+            p2++
+            countSameNum++
+        }
+
+        slc = append(slc, [2]int{countSameNum, int(str[p1]-'0')})
+        p1 = p2
+    }
+
+    return slc
+}
+
+func strRLE(slc [][2]int) string {
+    res := ""
+    for _, v := range slc {
+        res += strconv.Itoa(v[0]) + strconv.Itoa(v[1])
+    }
+
+    return res
+}
