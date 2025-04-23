209cdfbc4c570eeacad8c5851d6154fe78fb238c
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-04-23 23:54:45 +0700

    Time: 0 ms (100%), Space: 3.9 MB (68.97%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.go
@@ -0,0 +1,31 @@
+func countLargestGroup(n int) int {
+    m := map[int]int{}
+    for i := 1; i <= n; i++ {
+        m[digitSum(i)]++
+    }
+
+    count, maxFreq := 0, 0
+    for _, freq := range m {
+        if freq > maxFreq {
+            maxFreq = freq
+        }
+    }
+
+    for _, freq := range m {
+        if freq == maxFreq {
+            count++
+        }
+    }
+
+    return count
+}
+
+func digitSum(n int) int {
+    sum := 0
+    for n != 0 {
+        sum += n % 10
+        n /= 10
+    }
+
+    return sum
+}
