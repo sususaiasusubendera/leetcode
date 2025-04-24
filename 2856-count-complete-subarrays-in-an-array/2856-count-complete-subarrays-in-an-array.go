f5d7ec115bdc532e5568d03455e36ec4f971d5a4
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-04-25 00:38:12 +0700

    Time: 593 ms (13.64%), Space: 10.4 MB (31.82%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.go
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.go
@@ -1,31 +1,25 @@
 func countCompleteSubarrays(nums []int) int {
-	res := 0
-	cnt := make(map[int]int)
-	n := len(nums)
-	right := 0
-	distinct := make(map[int]bool)
-	for _, num := range nums {
-		distinct[num] = true
-	}
-	distinctCount := len(distinct)
-	for left := 0; left < n; left++ {
-		if left > 0 {
-			remove := nums[left-1]
-			cnt[remove]--
-			if cnt[remove] == 0 {
-				delete(cnt, remove)
-			}
-		}
-		for right < n && len(cnt) < distinctCount {
-			add := nums[right]
-			cnt[add]++
-			right++
-		}
-		if len(cnt) == distinctCount {
-			res += (n - right + 1)
-		}
-	}
-	return res
+    n := len(nums)
+    mapUnique := map[int]bool{}
+    for _, num := range nums {
+        mapUnique[num] = true
+    }
+    
+    count := 0
+    totalUnique := len(mapUnique)
+    for left := 0; left < n; left++ {
+        mapFreq := map[int]int{}
+        for right := left; right < n; right++ {
+            mapFreq[nums[right]]++
+            if len(mapFreq) == totalUnique {
+                count++
+            }
+        }
+    }
+
+    return count
 }
 
-// notice me senpai
+// sliding window
+// time: O(n^2)
+// space: O(n)
