dd0b6a39611b63fa8a6c44291cb6ac85c41d8bbe
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-05-11 18:00:24 +0700

    Time: 135 ms (22.22%), Space: 11.2 MB (33.33%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.go
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.go
@@ -1,32 +1,33 @@
 func minSum(nums1 []int, nums2 []int) int64 {
-	var sum1, sum2 int64
-	var zero1, zero2 int
+	sum1, sum2 := 0, 0
+    countZero1, countZero2 := 0, 0
+    for _, num := range nums1 {
+        sum1 += num
+        if num == 0 {
+            sum1++
+            countZero1++
+        }
+    }
 
-	for _, num := range nums1 {
-		sum1 += int64(num)
-		if num == 0 {
-			sum1++
-			zero1++
-		}
-	}
+    for _, num := range nums2 {
+        sum2 += num
+        if num == 0 {
+            sum2++
+            countZero2++
+        }
+    }
 
-	for _, num := range nums2 {
-		sum2 += int64(num)
-		if num == 0 {
-			sum2++
-			zero2++
-		}
-	}
+    if ((sum1 < sum2) && countZero1 == 0) || ((sum2 < sum1) && countZero2 == 0) {
+        return -1
+    }
 
-	if (zero1 == 0 && sum2 > sum1) || (zero2 == 0 && sum1 > sum2) {
-		return -1
-	}
+    if sum1 > sum2 {
+        return int64(sum1)
+    }
 
-	if sum1 > sum2 {
-		return sum1
-	}
-	return sum2
+    return int64(sum2)
 }
 
-// editorial
-// notice me senpai
+// greedy
+// time: O(n + m)
+// space: O(1)
