22fe274ba4edc531366d03bc3f9cde468ea555fa
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-05-17 17:23:09 +0700

    Time: 0 ms (100%), Space: 4 MB (97.19%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.go
@@ -0,0 +1,13 @@
+func sortColors(nums []int)  {
+    for i := 0; i < len(nums)-1; i++ {
+        for j := i+1; j < len(nums); j++ {
+            if nums[i] > nums[j] {
+                nums[i], nums[j] = nums[j], nums[i]
+            }
+        }
+    }
+}
+
+// bubble sort
+// time: O(n^2)
+// space: O(1)
