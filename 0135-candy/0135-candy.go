b13b7b060b7f743ac5377046466b54e19ccaae1e
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-06-03 09:06:19 +0700

    Time: 0 ms (100%), Space: 8.3 MB (39.41%) - LeetHub

--- 0135-candy/0135-candy.go
+++ 0135-candy/0135-candy.go
@@ -1,24 +1,34 @@
-// greedy single-pass
 func candy(ratings []int) int {
-    candies := 1
-    up, down, peak := 0, 0, 0
+    candies := make([]int, len(ratings))
+    for i := 0; i < len(candies); i++ {
+        candies[i] = 1
+    }
+
+    // check from left to right
     for i := 1; i < len(ratings); i++ {
         if ratings[i] > ratings[i-1] {
-            up++
-            down = 0
-            candies += up + 1
-            peak = up
-        } else if ratings[i] < ratings[i-1] {
-            down++
-            up = 0
-            candies += down
-            if peak < down {
-                candies++
-            }
-        } else {
-            up, down, peak = 0, 0, 0
-            candies++
+            candies[i] = candies[i-1] + 1
         }
     }
-    return candies
+
+    // check from right to left
+    for i := len(ratings)-2; i >= 0; i-- {
+        if (ratings[i] > ratings[i+1]) && (candies[i] <= candies[i+1]) {
+            // add a candy if candies[i] is not more than candies[i+1]
+            candies[i] = candies[i+1] + 1
+        }
+    }
+
+    result := 0
+    for _, candy := range candies {
+        result += candy
+    }
+
+    return result
 }
+
+// greedy two-pass
+// time: O(n)
+// space: O(n)
+
+// DCC june 2025 day 2
