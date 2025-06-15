854b72cbe6ad3d80e3bf00e422bc4391056a7700
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-06-15 15:49:07 +0700

    Time: 0 ms (100%), Space: 3.9 MB (42.42%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.go
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.go
@@ -1,19 +1,68 @@
 func minMaxDifference(num int) int {
-	s := strconv.Itoa(num)
-	t := s
-	pos := 0
-	for pos < len(s) && s[pos] == '9' {
-		pos++
-	}
-	if pos < len(s) {
-		s = strings.ReplaceAll(s, string(s[pos]), "9")
-	}
-	t = strings.ReplaceAll(t, string(t[0]), "0")
-	max, _ := strconv.Atoi(s)
-	min, _ := strconv.Atoi(t)
-	return max - min
+	digits := intToDigits(num)
+    
+    // find max num
+    // change the first non-nine digit (and all digits that are the same as it) in digits to nine
+    maxDigits := make([]int, len(digits))
+    copy(maxDigits, digits)
+    firstNonNine := -1
+    for _, digit := range digits {
+        if digit != 9 {
+            firstNonNine = digit
+            break
+        }
+    }
+    if firstNonNine != -1 {
+        for i := 0; i < len(maxDigits); i++ {
+            if maxDigits[i] == firstNonNine {
+                maxDigits[i] = 9
+            }
+        }
+    }
+
+    // find min num
+    // change the first non-zero digit (and all digits that are the same as it) in digits to zero
+    minDigits := make([]int, len(digits))
+    copy(minDigits, digits)
+    firstNonZero := -1
+    for _, digit := range digits {
+        if digit != 0 {
+            firstNonZero = digit
+            break
+        }
+    }
+    if firstNonZero != -1 {
+        for i := 0; i < len(minDigits); i++ {
+            if minDigits[i] == firstNonZero {
+                minDigits[i] = 0
+            }
+        }
+    }
+
+    maxNum := digitsToInt(maxDigits)
+    minNum := digitsToInt(minDigits)
+
+    return maxNum - minNum
 }
 
-// editorial
-// would
-// notice me senpai!
+// time: O(n)
+// space: O(n)
+
+func intToDigits(num int) []int {
+    digits := []int{}
+    for num > 0 {
+        digits = append([]int{num%10}, digits...)
+        num /= 10
+    }
+
+    return digits
+}
+
+func digitsToInt(digits []int) int {
+    num := 0
+    for _, digit := range digits {
+        num = num*10 + digit
+    }
+
+    return num
+}
