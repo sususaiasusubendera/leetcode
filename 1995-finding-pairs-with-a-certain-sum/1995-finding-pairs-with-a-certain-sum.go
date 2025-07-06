f77822d9852ab7f72854124a698213237a216d74
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-07-06 21:00:31 +0700

    Time: 74 ms (75%), Space: 16.6 MB (100%) - LeetHub

---
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.go
@@ -0,0 +1,46 @@
+type FindSumPairs struct {
+    nums1 []int
+    nums2 []int
+    count map[int]int
+}
+
+
+func Constructor(nums1 []int, nums2 []int) FindSumPairs {
+    count := make(map[int]int)
+    for _, num := range nums2 {
+        count[num]++
+    }
+
+    return FindSumPairs{
+        nums1: nums1,
+        nums2: nums2,
+        count: count,
+    }
+}
+
+
+func (this *FindSumPairs) Add(index int, val int)  {
+    oldVal := this.nums2[index]
+    this.count[oldVal]--
+    this.nums2[index] += val
+    this.count[this.nums2[index]]++
+}
+
+
+func (this *FindSumPairs) Count(tot int) int {
+    result := 0
+    for _, num := range this.nums1 {
+        rest := tot - num
+        result += this.count[rest]
+    }
+
+    return result
+}
+
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * obj := Constructor(nums1, nums2);
+ * obj.Add(index,val);
+ * param_2 := obj.Count(tot);
+ */
