6a3b3c900038ec6da66b2066a1c9f4b38a77c2c3
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-08-03 18:54:17 +0700

    Time: 27 ms (100%), Space: 14.4 MB (50%) - LeetHub

--- 2689-rearranging-fruits/2689-rearranging-fruits.go
+++ 2689-rearranging-fruits/2689-rearranging-fruits.go
@@ -1,47 +1,50 @@
 func minCost(basket1 []int, basket2 []int) int64 {
 	freq := map[int]int{}
-	m := math.MaxInt
-	for _, b := range basket1 {
-		freq[b]++
-		if b < m {
-			m = b
-		}
-	}
-	for _, b := range basket2 {
-		freq[b]--
-		if b < m {
-			m = b
-		}
-	}
+    minCost := int(1e9) // from constraints 1 <= basket1[i], basket2[i] <= 10^9
 
-	var merge []int
-	for k, c := range freq {
-		if c%2 != 0 {
-			return -1
-		}
-		for i := 0; i < abs(c)/2; i++ {
-			merge = append(merge, k)
-		}
-	}
+    for _, cost := range basket1 {
+        freq[cost]++
+        if cost < minCost {
+            minCost = cost
+        }
+    }
 
-	sort.Ints(merge)
-	var res int64
-	for i := 0; i < len(merge)/2; i++ {
-		if 2*m < merge[i] {
-			res += int64(2 * m)
-		} else {
-			res += int64(merge[i])
-		}
-	}
-	return res
+    for _, cost := range basket2 {
+        freq[cost]--
+        if cost < minCost {
+            minCost = cost
+        }
+    }
+
+    swap := []int{}
+    for k, v := range freq {
+        if v % 2 != 0 {
+            return -1
+        }
+
+        for i := 0; i < abs(v)/2; i++ {
+            swap = append(swap, k)
+        }
+    }
+
+    sort.Ints(swap)
+    result := 0
+    for i := 0; i < len(swap)/2; i++ {
+        if 2*minCost < swap[i] {
+            result += 2*minCost
+            continue
+        }
+
+        result += swap[i]
+    }
+
+    return int64(result)
 }
 
-// editorial
-// NOTICE ME SENPAI!!!
+func abs(n int) int {
+    if n < 0 {
+        return -n
+    }
 
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
+    return n
 }
