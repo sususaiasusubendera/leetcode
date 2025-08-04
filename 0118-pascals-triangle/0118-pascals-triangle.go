4678c2c88236929064c3375b870f698d3f4c6694
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-08-05 00:23:16 +0700

    Time: 2 ms (1.73%), Space: 4.3 MB (50.46%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.go
+++ 0118-pascals-triangle/0118-pascals-triangle.go
@@ -1,20 +1,20 @@
 func generate(numRows int) [][]int {
 	result := make([][]int, numRows)
 	result[0] = []int{1}
-
+	
 	if numRows == 1 {
 		return result
 	}
-
+	
 	result[1] = []int{1, 1}
 	for i := 2; i < numRows; i++ {
-        row := []int{1}
-        for j := 1; j < i; j++ {
-            row = append(row, result[i-1][j-1]+result[i-1][j])
-        }
-        row = append(row, 1)
-
-        result[i] = row  
+		row := []int{1}
+		for j := 1; j < i; j++ {
+	   		row = append(row, result[i-1][j-1]+result[i-1][j])
+		}
+		row = append(row, 1)
+	
+		result[i] = row  
 	}
 
     return result
