fd45d7f45ac17371d5c33d3e58adf3742edbbdca
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-08-09 22:14:20 +0700

    Time: 68 ms (0%), Space: 9 MB (0%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.go
+++ 0826-soup-servings/0826-soup-servings.go
@@ -1,39 +1,38 @@
 func soupServings(n int) float64 {
-    if n > 5000 {
-        return 1
+    serving := (n + 24) / 25 // ceiling
+    memo := map[[2]int]float64{}
+
+    var dp func(i, j int) float64
+    dp = func(i, j int) float64 {
+        if i <= 0 && j <= 0 {
+            return 0.5
+        }
+        if i <= 0 {
+            return 1
+        }
+        if j <= 0 {
+            return 0
+        }
+
+        key := [2]int{i, j}
+        if val, ok := memo[key]; ok {
+            return val
+        }
+
+        memo[key] = (dp(i-4, j) + dp(i-3, j-1) + dp(i-2, j-2) + dp(i-1, j-3)) / 4
+        return memo[key]
     }
-    cache := make(map[int]float64)
-    probability := calculateProbability(n, n, cache)
-    return probability
+
+    // optimization
+    for i := 1; i <= serving; i++ {
+        if dp(i, i) > 1 - 1e-5 {
+            return 1
+        }
+    }
+
+    return dp(serving, serving)
 }
 
-// notice me senpai!!!
 // dp
-
-func calculateProbability(a int, b int, cache map[int]float64) float64 {
-    key := getKey(a, b)
-    value, exists := cache[key]
-    if exists {
-        return value
-    }
-    if a <= 0 && b <= 0 {
-        return 0.5
-    }
-    if a <= 0 && b > 0 {
-        return 1
-    }
-    if a > 0 && b <= 0 {
-        return 0
-    }
-    p1 := calculateProbability(a - 100, b, cache)
-    p2 := calculateProbability(a - 75, b - 25, cache)
-    p3 := calculateProbability(a - 50, b - 50, cache)
-    p4 := calculateProbability(a - 25, b - 75, cache)
-    result := 0.25 * (p1 + p2 + p3 + p4)
-    cache[key] = result
-    return result
-}
-
-func getKey(a int, b int) int {
-    return (a << 15) + b
-}
+// time: O(n^2)
+// space: O(n^2)
