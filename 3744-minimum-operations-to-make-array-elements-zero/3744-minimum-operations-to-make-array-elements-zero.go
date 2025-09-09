873a3c3b4e7f3fc6f7d837c1f4002e47b7bcbf47
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-09-09 22:51:54 +0700

    Time: 15 ms (60.44%), Space: 19.5 MB (85.06%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.go
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.go
@@ -1,28 +1,39 @@
-func get(num int) int64 {
-	var cnt int64
-	i := 1
-	base := 1
-
-	for base <= num {
-		end := base*2 - 1
-		if end > num {
-			end = num
-		}
-		cnt += int64((i+1)/2) * int64(end-base+1)
-		i++
-		base *= 2
-	}
-	return cnt
-}
-
 func minOperations(queries [][]int) int64 {
-	var res int64
-	for _, q := range queries {
-		count1 := get(q[1])
-		count2 := get(q[0] - 1)
-		res += (count1 - count2 + 1) / 2
-	}
-	return res
+    var f func(n int) int
+    f = func(n int) int {
+        if n == 0 { return 0 }
+
+        total := 0
+        step := 1 // steps needed for current block
+        start := 1 // start of the block
+        // each block's length: 3 * 4^(k-1)
+        length := 3 // length of the first block (1..3)
+        for start <= n {
+            end := start + length - 1
+            if end > n { end = n } // cut, if the block > n
+
+            count := end - start + 1 // total numbers in the block
+            total += count * step // steps contribution from the block
+
+            // next block
+            step++
+            start *= 4
+            length *= 4
+        }
+
+        return total
+    }
+
+    ops := 0
+    for _, query := range queries {
+        l, r := query[0], query[1]
+        steps := f(r) - f(l-1) // total steps [l..r]
+        ops += (steps + 1) / 2 // 1 op can process 2 numbers
+    }
+
+    return int64(ops)
 }
 
-// editorial
+// array, math
+// time: O(qlog(n))
+// space: O(1)
