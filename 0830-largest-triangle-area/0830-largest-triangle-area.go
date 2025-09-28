2b86b6ba40e8b5b2101ab3129cf2042ec822238a
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-09-28 22:15:40 +0700

    Time: 0 ms (100%), Space: 4.1 MB (33.33%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.go
+++ 0830-largest-triangle-area/0830-largest-triangle-area.go
@@ -1,19 +1,38 @@
-func calcArea(a, b, c []int) int {
-	return a[0]*(b[1]-c[1]) + b[0]*(c[1]-a[1]) + c[0]*(a[1]-b[1])
-}
-
 func largestTriangleArea(points [][]int) float64 {
-	n := len(points)
-	result := 0
-	for i := 0; i < n-2; i++ {
-		for j := i + 1; j < n-1; j++ {
-			for k := j + 1; k < n; k++ {
-				area := calcArea(points[i], points[j], points[k])
-				result = max(result, area, -area)
+	max := 0.0
+	for i := 0; i < len(points)-2; i++ {
+		for j := i + 1; j < len(points)-1; j++ {
+			for k := j + 1; k < len(points); k++ {
+				area := triangleArea(points[i], points[j], points[k])
+				if area > max {
+					max = area
+				}
 			}
 		}
 	}
-	return 0.5 * float64(result)
+
+	return max
 }
 
-// solution
+// math
+// time: O(n^3)
+// space: O(1)
+
+func triangleArea(a, b, c []int) float64 {
+	xa, ya := a[0], a[1]
+	xb, yb := b[0], b[1]
+	xc, yc := c[0], c[1]
+
+	p1 := float64(xa) * (float64(yb) - float64(yc))
+	p2 := float64(xb) * (float64(yc) - float64(ya))
+	p3 := float64(xc) * (float64(ya) - float64(yb))
+
+	return 0.5 * abs(p1+p2+p3)
+}
+
+func abs(n float64) float64 {
+	if n < 0 {
+		return -n
+	}
+	return n
+} 
