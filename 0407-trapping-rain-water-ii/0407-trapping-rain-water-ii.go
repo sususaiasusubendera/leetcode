4fe5a94aa986ab1ed4454af1f5bd86981d569da1
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-10-03 22:11:04 +0700

    Time: 20 ms (87.5%), Space: 9.6 MB (37.5%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.go
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.go
@@ -1,83 +1,73 @@
 func trapRainWater(heightMap [][]int) int {
-    m, n := len(heightMap), len(heightMap[0])
-    if m < 3 || n < 3 {
-        return 0 // no water can be trapped in a map smaller than 3x3
-    }
+	m, n := len(heightMap), len(heightMap[0])
+	if m < 3 || n < 3 {
+		return 0
+	} // no water can be trapped in a heightMap smaller than 3x3
 
-    // visited matrix
-    visited := make([][]bool, m)
-    for i := range visited {
-        visited[i] = make([]bool, n)
-    }
+	visited := make([][]bool, m)
+	for i := range visited {
+		visited[i] = make([]bool, n)
+	}
 
-    // MinHeap to store the boundary cells
-    h := &MinHeap{}
-    heap.Init(h)
-    // add all boundary cells to the heap
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            if i == 0 || i == m-1 || j == 0 || j == n-1 {
-                heap.Push(h, Cell{i, j, heightMap[i][j]})
-                visited[i][j] = true
-            }
-        }
-    }
+	// add all outer layer cells of the heightMap to visited and heap
+	h := &MinHeap{}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || i == m-1 || j == 0 || j == n-1 {
+				heap.Push(h, Cell{i, j, heightMap[i][j]})
+				visited[i][j] = true
+			}
+		}
+	}
 
-    // directions for exploring neighbors
-    directions := []struct{dr, dc int}{
-        {-1, 0}, {1, 0}, {0, -1}, {0, 1},
-    }
+	dirs := [][]int{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} // up, right, down, left
+	water := 0
+	for h.Len() > 0 {
+		cell := heap.Pop(h).(Cell)
+		for _, dir := range dirs {
+			newRow, newCol := cell.row+dir[0], cell.col+dir[1]
+			if newRow > 0 && newRow < m-1 && newCol > 0 && newCol < n-1 && !visited[newRow][newCol] {
+				neighborHeight := heightMap[newRow][newCol]
+				if neighborHeight < cell.height {
+					water += cell.height - neighborHeight
+					neighborHeight = cell.height
+				}
 
-    water := 0
-    for h.Len() > 0 {
-        cell := heap.Pop(h).(Cell)
-        for _, dir := range directions {
-            newRow, newCol := cell.row+dir.dr, cell.col+dir.dc
-            if newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited[newRow][newCol] {
-                visited[newRow][newCol] = true
-                // if the current cell is higher than the neighbor, water can be trapped
-                water += max(0, cell.height-heightMap[newRow][newCol])
-                // push the neighbor into the heap with the maximum height
-                heap.Push(h, Cell{newRow, newCol, max(cell.height, heightMap[newRow][newCol])})
-            }
-        }
-    }
+				heap.Push(h, Cell{newRow, newCol, neighborHeight})
+				visited[newRow][newCol] = true
+			}
+		}
+	}
 
-    return water
+	return water
 }
 
-// UTILS
-func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
-}
+// array, bfs, heap, matrix
+// time: O(mn log(mn))
+// space: mn
 
-// TYPES AND METHODS
+// min heap implementation
 type Cell struct {
-    row, col, height int
+	row    int
+	col    int
+	height int
 }
 
-// MinHeap
 type MinHeap []Cell
 
-func (h MinHeap) Len() int {
-    return len(h)
-}
-func (h MinHeap) Less(i, j int) bool {
-    return h[i].height < h[j].height
-}
-func (h MinHeap) Swap(i, j int) {
-    h[i], h[j] = h[j], h[i]
-}
-func (h *MinHeap) Push(x interface{}) {
-    *h = append(*h, x.(Cell))
-}
-func (h *MinHeap) Pop() interface{} {
-    old := *h
-    n := len(old)
-    cell := old[n-1]
-    *h = old[:n-1]
-    return cell
+func (h *MinHeap) Len() int { return len(*h) }
+
+func (h *MinHeap) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *MinHeap) Less(i, j int) bool { return (*h)[i].height < (*h)[j].height }
+
+func (h *MinHeap) Push(x any) { *h = append(*h, x.(Cell)) }
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+
+	return x
 }
