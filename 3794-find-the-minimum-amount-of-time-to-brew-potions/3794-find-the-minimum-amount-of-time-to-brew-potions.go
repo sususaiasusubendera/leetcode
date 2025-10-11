204924e291c01c622ae94ac3eb4059a284d44c3f
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-10-11 15:23:20 +0700

    Time: 92 ms (61.01%), Space: 6.1 MB (53.21%) - LeetHub

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.go
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.go
@@ -1,9 +1,10 @@
 func minTime(skill []int, mana []int) int64 {
 	n, m := len(skill), len(mana)
-	wizardTime := make([]int, len(skill)) // store the finish time of wizard[i], used as the next potion's start time
+	wizardTime := make([]int, len(skill)) // stores the time when each wizard can start brewing the next potion
 	for j := 0; j < m; j++ {
 		currTime := 0
 		for i := 0; i < n; i++ {
+            // wait until this wizard is available
 			if currTime < wizardTime[i] {
 				currTime = wizardTime[i]
 			} 
@@ -11,6 +12,7 @@ func minTime(skill []int, mana []int) int64 {
 			currTime += skill[i] * mana[j]
 		}
 
+        // backtrack to update when each wizard can start the next potion
 		wizardTime[n-1] = currTime
 		for i := n - 2; i >= 0; i-- {
 			wizardTime[i] = wizardTime[i+1] - (skill[i+1] * mana[j])
@@ -20,4 +22,6 @@ func minTime(skill []int, mana []int) int64 {
 	return int64(wizardTime[n-1])
 }
 
-// notice me senpai
+// array, dp(?), simulation
+// time: O(mn)
+// space: O(n)
