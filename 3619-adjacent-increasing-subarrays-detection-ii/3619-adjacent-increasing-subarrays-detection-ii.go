d09ea1117d77e3d5ea22ae8c406fd53bb9b159bc
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-10-15 12:40:23 +0700

    Time: 189 ms (85.71%), Space: 14.4 MB (57.14%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.go
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.go
@@ -1,36 +1,25 @@
 func maxIncreasingSubarrays(nums []int) int {
-	f, g := make([]int, len(nums)), make([]int, len(nums))
-	f[0] = 1
-	g[len(g)-1] = 1
-    
-	for i := 1; i < len(nums); i++ {
-		if nums[i] > nums[i-1] {
-			f[i] = f[i-1] + 1
-		} else {
-			f[i] = 1
-		}
-	}
-
-	for i := len(nums) - 2; i >= 0; i-- {
-        if nums[i] < nums[i+1] {
-            g[i] = g[i+1] + 1
-        } else {
-            g[i] = 1
-        }
-	}
-
-    ans := 0
+	ans := 0
+    prevCount := 0
+    count := 1
     for i := 1; i < len(nums); i++ {
-        // f[i-1]: len(nums[a..a+k-1]), g[i]: len(b..b+k-1); b = a + k
-        ans = max(ans, min(f[i-1], g[i]))
+        if nums[i] > nums[i-1] {
+            count++
+        } else {
+            prevCount = count
+            count = 1
+        }
+
+        ans = max(ans, min(prevCount, count)) // adjacent 2 increasing subarrays
+        ans = max(ans, count/2) // 1 increasing subarray, split it into 2
     }
 
     return ans
 }
 
-// dp (bot-up) two-pass
+// greedy
 // time: O(n)
-// space: O(n)
+// space: O(1)
 
 func max(a, b int) int {
     if a > b { return a }
