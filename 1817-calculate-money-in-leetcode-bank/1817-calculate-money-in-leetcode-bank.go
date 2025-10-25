da8875fadb50fbcbc757ef5337e5fd4c98d9ff98
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-10-25 13:17:27 +0700

    Time: 0 ms (100%), Space: 3.9 MB (74.29%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.go
@@ -0,0 +1,17 @@
+func totalMoney(n int) int {
+	weeks := n / 7
+	daysRemain := n % 7
+	monday := 1
+	money := 0
+	for monday <= weeks {
+		money += (7 * (2*monday + 6)) / 2
+		monday++
+	}
+	money += (daysRemain * (2*monday + daysRemain - 1)) / 2
+
+	return money
+}
+
+// math
+// O(n)
+// O(1)
