aae5c2fd389f57785f6f98dc55e77aced6c42c11
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-11-08 23:11:17 +0700

    Time: 44 ms (100%), Space: 12.6 MB (25%) - LeetHub

--- 2618-maximize-the-minimum-powered-city/2618-maximize-the-minimum-powered-city.go
+++ 2618-maximize-the-minimum-powered-city/2618-maximize-the-minimum-powered-city.go
@@ -1,61 +1,78 @@
 func maxPower(stations []int, r int, k int) int64 {
-	n := len(stations)
-	cnt := make([]int64, n+1)
-	for i := 0; i < n; i++ {
+	// compute the base power for each city (total power currently received)
+	base := make([]int, len(stations))
+	prefix := make([]int, len(stations)+1)
+	for i := 0; i < len(stations); i++ {
+		prefix[i+1] = prefix[i] + stations[i]
+	}
+	for i := 0; i < len(stations); i++ {
 		left := max(0, i-r)
-		right := min(n, i+r+1)
-		cnt[left] += int64(stations[i])
-		cnt[right] -= int64(stations[i])
+		right := min(len(stations)-1, i+r)
+		base[i] = prefix[right+1] - prefix[left]
 	}
 
-	minVal := int64(stations[0])
-	sumTotal := int64(0)
-	for _, s := range stations {
-		if int64(s) < minVal {
-			minVal = int64(s)
-		}
-		sumTotal += int64(s)
+	// binary search
+	low := 0
+	total := 0
+	for _, station := range stations {
+		total += station
 	}
-
-	lo, hi := minVal, sumTotal+int64(k)
-	var res int64 = 0
-
-	for lo <= hi {
-		mid := lo + (hi-lo)/2
-		if check(cnt, mid, r, k) {
+	high := total + k
+	res := 0
+	for low <= high {
+		mid := (low + high) / 2
+		if canAchieve(base, r, k, mid) {
 			res = mid
-			lo = mid + 1
+			low = mid + 1
 		} else {
-			hi = mid - 1
+			high = mid - 1
 		}
 	}
-	return res
+
+	return int64(res)
 }
 
-func check(cnt []int64, val int64, r int, k int) bool {
-	n := len(cnt) - 1
-	diff := make([]int64, len(cnt))
-	copy(diff, cnt)
-	var sum int64 = 0
-	remaining := int64(k)
+// array, binary search, diff arr, greedy, prefix sum
+// time: O(n.log(sum(stations)+k))
+// space: O(n)
 
-	for i := 0; i < n; i++ {
-		sum += diff[i]
-		if sum < val {
-			add := val - sum
-			if remaining < add {
+func canAchieve(base []int, r int, k int, target int) bool {
+	diff := make([]int, len(base)+1)
+	currAdd := 0
+	used := 0
+	for i := 0; i < len(base); i++ {
+		currAdd += diff[i]        // current active diffArr effect
+		curr := base[i] + currAdd // current actual power of city i
+		if curr < target {
+			need := target - curr
+			used += need
+			if used > k {
 				return false
 			}
-			remaining -= add
-			end := min(n, i+2*r+1)
-			diff[end] -= add
-			sum += add
+
+			pos := min(i+r, len(base)-1)
+			left := max(0, pos-r)
+			right := min(len(base)-1, pos+r)
+
+			diff[left] += need
+			diff[right+1] -= need
+			currAdd += need
 		}
 	}
-
 	return true
 }
 
-// EDITORIAL
-// NOTICE ME SENPAI!!!
-// binary search, diff array
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
