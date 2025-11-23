79d7d01a9c4a66cc5a82b9f3acd0258b1f7c24b7
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-11-23 14:14:48 +0700

    Time: 0 ms (100%), Space: 8.4 MB (100%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.go
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.go
@@ -1,32 +1,22 @@
 func maxSumDivThree(nums []int) int {
-	dp := make([][]int, len(nums)+1)
-	dp[0] = []int{0, -1, -1} // remainder x % 3 (0, 1, 2); sum = 0 -> r = 0
-
-	for i := 1; i <= len(nums); i++ {
-		num := nums[i-1]
-		rem := num % 3
-		dp[i] = []int{-1, -1, -1}
+	dp := []int{0, -1, -1} // remainder x % 3 (0, 1, 2); sum = 0 -> r = 0
+	for _, num := range nums {
+		prev := make([]int, 3)
+		copy(prev, dp)
 		for r := 0; r < 3; r++ {
-			// skip num
-			if dp[i-1][r] != -1 {
-				dp[i][r] = max(dp[i][r], dp[i-1][r])
+			newR := (r + num) % 3
+			if prev[r] != -1 { //only valid prev[r] (total for "remainder = r" state)
+				dp[newR] = max(dp[newR], prev[r]+num)
 			}
-
-			// take num
-			if dp[i-1][r] != -1 {
-				newR := (r + rem) % 3
-				dp[i][newR] = max(dp[i][newR], dp[i-1][r]+num)
-			}
-
 		}
 	}
 
-	return dp[len(nums)][0]
+	return dp[0]
 }
 
 // dp (bot-up)
 // time: O(n)
-// space: O(n)
+// space: O(1)
 
 func max(a, b int) int {
 	if a > b {
