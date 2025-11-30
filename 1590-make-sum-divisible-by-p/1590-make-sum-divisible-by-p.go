2e023b9de8457086485d8f9527fa23edbae976a0
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-11-30 18:11:11 +0700

    Time: 35 ms (74.07%), Space: 14.2 MB (55.56%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.go
@@ -0,0 +1,37 @@
+func minSubarray(nums []int, p int) int {
+    sumMod := 0 // stores sum of nums % p, so that it doesn't overflow
+    for _, num := range nums {
+        sumMod = (sumMod + num) % p
+    }
+
+    need := sumMod
+    if need == 0 { return 0 }
+
+    m := map[int]int{} // map prefix[i] (in mod p) to i
+    m[0] = 0 // prefix[0] = 0 at index 0
+    res := len(nums) // length of the smallest subarray that need to remove
+    prefix := 0
+    for i := 1; i <= len(nums); i++ {
+        prefix = (prefix + nums[i-1]) % p
+        curr := prefix
+
+        // let sum of subarray[l..r+1] = sub, and prefix sum = P
+        // sub = P[r+1] - P[l]; sub % p == need
+        // (P[r+1] - P[l]) % p == need) -> ((curr - P[l]) % p == need)
+        // P[l] = (curr - need + p) % p; + p so that P[l] always positive [0..p)
+        target := (curr - need + p) % p
+        if idx, ok := m[target]; ok {
+            // i - idx = length of subarray[l..r+1]
+            if i - idx < res { res = i - idx }
+        }
+
+        m[curr] = i
+    }
+
+    if res == len(nums) { return -1 }
+    return res
+}
+
+// array, hash map, prefix sum 
+// time: O(n)
+// space: O(n)
