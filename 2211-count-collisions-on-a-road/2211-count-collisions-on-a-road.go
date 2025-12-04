a5410e1322fa32b56e61b5fd7a0456fe655bb5b7
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-12-04 20:54:34 +0700

    Time: 21 ms (14.29%), Space: 9.6 MB (14.29%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.go
@@ -0,0 +1,47 @@
+func countCollisions(directions string) int {
+    count := 0
+    stack := []rune{}
+    for _, d := range directions {
+        if len(stack) > 0 {
+            if stack[len(stack)-1] == 'R' {
+                if d == 'L' {
+                    count += 2
+                    stack = stack[:len(stack)-1] // pop
+                    for len(stack) > 0 && stack[len(stack)-1] == 'R' {
+                        count += 1 // R collides with S (RL -> S)
+                        stack = stack[:len(stack)-1] // pop
+                    }
+                    stack = append(stack, 'S')
+                } else if d == 'S' {
+                    for len(stack) > 0 && stack[len(stack)-1] == 'R' {
+                        count += 1 // R collides with S
+                        stack = stack[:len(stack)-1] // pop
+                    }
+                    stack = append(stack, 'S')
+                } else {
+                    stack = append(stack, 'R')
+                }
+            } else if stack[len(stack)-1] == 'S' {
+                if d == 'L' {
+                    count += 1
+                    stack = append(stack, 'S')
+                } else if d == 'R' {
+                    stack = append(stack, 'R')
+                } else {
+                    stack = append(stack, 'S')
+                }
+            } else if stack[len(stack)-1] == 'L' {
+                stack = append(stack, d)
+            }
+            continue
+        }
+
+        stack = append(stack, d)
+    }
+
+    return count
+}
+
+// simulation, stack, string
+// time: O(n)
+// space: O(n)
