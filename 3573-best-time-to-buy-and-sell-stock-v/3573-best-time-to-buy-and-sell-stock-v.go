231a7146183f3518dc9131b54fbb528bc6884ada
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-12-17 20:38:13 +0700

    Time: 3 ms (93.75%), Space: 6.4 MB (56.25%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.go
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.go
@@ -1,27 +1,21 @@
 func maximumProfit(prices []int, k int) int64 {
-	dp := make([][][]int64, len(prices))
-	for i := range dp {
-		dp[i] = make([][]int64, k+1)
-		for j := range dp[i] {
-			dp[i][j] = make([]int64, 3)
+	dp := make([][3]int64, len(prices))
+	// day 0
+	for i := 1; i <= k; i++ {
+        // OPEN
+		dp[i][1] = int64(-prices[0]) // normal: buy -> sell
+		dp[i][2] = int64(prices[0])  // short: sell -> buy
+	}
+
+	for i := 1; i < len(prices); i++ {
+		for j := k; j > 0; j-- {
+			dp[j][0] = max(dp[j][0], max(dp[j][1]+int64(prices[i]), dp[j][2]-int64(prices[i]))) // idle, CLOSE
+            dp[j][1] = max(dp[j][1], dp[j-1][0]-int64(prices[i])) // normal, OPEN
+            dp[j][2] = max(dp[j][2], dp[j-1][0]+int64(prices[i])) // short, OPEN
 		}
 	}
 
-    // day 0
-    for j := 1; j <= k; j++ {
-        dp[0][j][1] = int64(-prices[0]) // normal: buy -> sell
-        dp[0][j][2] = int64(prices[0]) // short: sell -> buy
-    }
-
-    for i := 1; i < len(prices); i++ {
-        for j := 1; j <= k; j++ {
-            dp[i][j][0] = max(dp[i-1][j][0], max(dp[i-1][j][1]+int64(prices[i]), dp[i-1][j][2]-int64(prices[i])))
-            dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-int64(prices[i]))
-            dp[i][j][2] = max(dp[i-1][j][2], dp[i-1][j-1][0]+int64(prices[i]))
-        }
-    }
-
-    return dp[len(prices)-1][k][0]
+	return dp[k][0]
 }
 
 func max(a, b int64) int64 {
@@ -31,6 +25,6 @@ func max(a, b int64) int64 {
 	return b
 }
 
-// array, dp (bot-up)
+// array, dp (bot-up rolling state)
 // time: O(nk)
-// space: O(nk)
+// space: O(k)
