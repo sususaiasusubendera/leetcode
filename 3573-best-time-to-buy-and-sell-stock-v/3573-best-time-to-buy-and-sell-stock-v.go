694fc09291e1e3bec94ffe61e3fac9cde88dd857
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2025-12-17 16:09:20 +0700

    Time: 206 ms (31.25%), Space: 64.5 MB (6.25%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.go
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.go
@@ -1,55 +1,27 @@
 func maximumProfit(prices []int, k int) int64 {
-	memo := make([][][]int64, len(prices))
-	for i := range memo {
-		memo[i] = make([][]int64, k+1)
-		for j := range memo[i] {
-			memo[i][j] = make([]int64, 3)
-			for s := range memo[i][j] {
-				memo[i][j][s] = -1
-			}
+	dp := make([][][]int64, len(prices))
+	for i := range dp {
+		dp[i] = make([][]int64, k+1)
+		for j := range dp[i] {
+			dp[i][j] = make([]int64, 3)
 		}
 	}
 
-	// i: day; j: transaction left; s: state
-	// state: 
-    // - 0: idle
-    // - 1: normal transaction (buy - sell)
-    // - 2: short selling transaction (sell -> buy)
-	var dfs func(int, int, int) int64
-	dfs = func(i, j, s int) int64 {
-		if j == 0 { // no transaction left
-			return 0
-		}
+    // day 0
+    for j := 1; j <= k; j++ {
+        dp[0][j][1] = int64(-prices[0]) // normal: buy -> sell
+        dp[0][j][2] = int64(prices[0]) // short: sell -> buy
+    }
 
-		if i == 0 { // day 0
-			if s == 0 { // idle
-				return 0
-			} else if s == 1 { // normal (buy -> sell); OPEN TRANSACTION
-				return -int64(prices[0]) // buy
-			} else if s == 2 { // short (sell -> buy); OPEN TRANSACTION
-				return int64(prices[0]) // sell
-			}
-		}
+    for i := 1; i < len(prices); i++ {
+        for j := 1; j <= k; j++ {
+            dp[i][j][0] = max(dp[i-1][j][0], max(dp[i-1][j][1]+int64(prices[i]), dp[i-1][j][2]-int64(prices[i])))
+            dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-int64(prices[i]))
+            dp[i][j][2] = max(dp[i-1][j][2], dp[i-1][j-1][0]+int64(prices[i]))
+        }
+    }
 
-		if memo[i][j][s] != -1 {
-			return memo[i][j][s]
-		}
-
-		p := prices[i]
-		res := int64(0)
-		if s == 0 { // idle; CLOSE TRANSACTION
-			res = max(dfs(i-1, j, 0), max(dfs(i-1, j, 1)+int64(p), dfs(i-1, j, 2)-int64(p)))
-		} else if s == 1 { // normal
-			res = max(dfs(i-1, j, 1), dfs(i-1, j-1, 0)-int64(p))
-		} else if s == 2 { // short
-			res = max(dfs(i-1, j, 2), dfs(i-1, j-1, 0)+int64(p))
-		}
-
-		memo[i][j][s] = res
-		return res
-	}
-
-	return dfs(len(prices)-1, k, 0)
+    return dp[len(prices)-1][k][0]
 }
 
 func max(a, b int64) int64 {
@@ -59,6 +31,6 @@ func max(a, b int64) int64 {
 	return b
 }
 
-// array, dp (top-down)
+// array, dp (bot-up)
 // time: O(nk)
 // space: O(nk)
