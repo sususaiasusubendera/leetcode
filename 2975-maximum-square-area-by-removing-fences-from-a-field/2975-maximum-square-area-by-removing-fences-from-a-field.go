feae089e36e2736e6a7c45bbfd0283cb0a092029
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-01-17 13:01:40 +0700

    Time: 504 ms (64.29%), Space: 16.8 MB (100%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.go
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.go
@@ -1,39 +1,40 @@
 func maximizeSquareArea(m int, n int, hFences []int, vFences []int) int {
-	hEdges := getEdges(hFences, m)
-	vEdges := getEdges(vFences, n)
+    // add unremovable fence (boundary) for hFences and vFences
+    hFences = append([]int{1}, append(hFences, m)...)
+    vFences = append([]int{1}, append(vFences, n)...)
 
-	var res int64 = 0
-	for e := range hEdges {
-		if _, exists := vEdges[e]; exists {
-			if int64(e) > res {
-				res = int64(e)
-			}
-		}
-	}
+    sort.Slice(hFences, func(i, j int) bool { return hFences[i] < hFences[j] })
+    sort.Slice(vFences, func(i, j int) bool { return vFences[i] < vFences[j] })
 
-	if res == 0 {
-		return -1
-	}
-	return int((res * res) % 1000000007)
+    hSet := map[int]struct{}{}
+    for i := 0; i < len(hFences)-1; i++ {
+        for j := i + 1; j < len(hFences); j++ {
+            s := hFences[j] - hFences[i]
+            if _, exists := hSet[s]; !exists {
+                hSet[s] = struct{}{}
+            }
+        }
+    }
+
+    maxS := -1 // max side
+    for i := 0; i < len(vFences)-1; i++ {
+        for j := i + 1; j < len(vFences); j++ {
+            s := vFences[j] - vFences[i]
+            if _, exists := hSet[s]; exists {
+                maxS = max(maxS, s)
+            }
+        }
+    }
+
+    if maxS == -1 {
+        return -1
+    }
+
+    const MOD = 1_000_000_007
+
+    return (maxS * maxS) % MOD
 }
 
-func getEdges(fences []int, border int) map[int]bool {
-	set := make(map[int]bool)
-	list := make([]int, len(fences))
-
-	copy(list, fences)
-	list = append(list, 1, border)
-	sort.Ints(list)
-
-	for i := 0; i < len(list); i++ {
-		for j := i + 1; j < len(list); j++ {
-			set[list[j]-list[i]] = true
-		}
-	}
-
-	return set
-}
-
-// editorial
 // array, hash map
-// NOTICE ME SENPAI
+// time: O(h^2 + v^2)
+// space: O(h^2 + h + v)
