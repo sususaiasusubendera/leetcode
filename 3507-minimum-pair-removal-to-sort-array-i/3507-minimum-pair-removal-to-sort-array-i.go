716e971880d311efd592b5e549ece0ad04d19c76
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-01-23 16:11:34 +0700

    Time: 0 ms (100%), Space: 4.4 MB (45.45%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.go
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.go
@@ -1,32 +1,40 @@
 func minimumPairRemoval(nums []int) int {
-	count := 0
-
-	for len(nums) > 1 {
-		isAscending := true
-		minSum := 1<<31 - 1
-		targetIndex := -1
-
-		for i := 0; i < len(nums)-1; i++ {
-			sum := nums[i] + nums[i+1]
-			if nums[i] > nums[i+1] {
-				isAscending = false
-			}
-			if sum < minSum {
-				minSum = sum
-				targetIndex = i
-			}
-		}
-
-		if isAscending {
-			break
-		}
-		count++
-		nums[targetIndex] = minSum
-		nums = append(nums[:targetIndex+1], nums[targetIndex+2:]...)
+	if len(nums) == 1 {
+		return 0
 	}
 
-	return count
+	ops := 0
+	for len(nums) > 1 {
+		sorted := true
+		minSum := 1_000_000_007
+		targetIdx := -1 // idx delete
+
+		for i := 1; i < len(nums); i++ {
+			if nums[i] < nums[i-1] {
+				sorted = false
+			}
+
+			sum := nums[i] + nums[i-1]
+			if sum < minSum {
+				minSum = sum
+				targetIdx = i - 1
+			}
+		}
+
+		if sorted {
+			break
+		}
+
+		if targetIdx > -1 {
+			ops++
+			nums[targetIdx] = minSum
+			nums = append(nums[:targetIdx+1], nums[targetIdx+2:]...)
+		}
+	}
+
+	return ops
 }
 
-// notice me senpai
-// editorial
+// array, simulation
+// time: O(n^2)
+// space: O(n)
