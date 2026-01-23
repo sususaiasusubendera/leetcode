a04d53883ea13102c41068a39f846d7be33bfcae
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-01-23 10:30:13 +0700

    Time: 0 ms (100%), Space: 5.3 MB (84.82%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.go
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.go
@@ -1,16 +1,33 @@
 func minBitwiseArray(nums []int) []int {
-	for i, x := range nums {
-		res := -1
-		d := 1
-		for (x & d) != 0 {
-			res = x - d
-			d <<= 1
-		}
-		nums[i] = res
-	}
-	return nums
+    // e.g. num = 23
+    // bin: 10111 (23)
+    // so,
+    // x: 10011 (19)
+    // x + 1: 10100 (20)
+    // x | (x + 1)
+    // 10011
+    // 10100 OR
+    // 10111 -> 19 OR 20 = 23
+
+    ans := make([]int, len(nums))
+    for i, num := range nums {
+        // ans[i] = -1 if lsb 0
+        if num & 1 == 0 {
+            ans[i] = -1
+            continue
+        }
+
+        // find the last bin digit 1 from lsb
+        d := 1 // single-bit mask
+        for (num & d) != 0 {
+            d <<= 1
+        }
+        d >>= 1
+        ans[i] = num - d
+    }
+
+    return ans
 }
 
-// editorial
-// notice me senpai
-// array, bit manipulation
+// time: O(n)
+// space: O(n)
