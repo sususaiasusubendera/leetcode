3423eacfde4e2e316fa2dc0fb3c995ae2d36187f
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-02-09 12:12:33 +0700

    Time: 0 ms (100%), Space: 4.2 MB (31.71%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    ans := []int{}
+
+    var dfs func(node *TreeNode)
+    dfs = func(node *TreeNode) {
+        if node == nil { return }
+
+        ans = append(ans, node.Val)
+        dfs(node.Left)
+        dfs(node.Right)
+    }
+
+    dfs(root)
+
+    return ans
+}
+
+// binary tree, dfs, stack, tree
+// time: O(n)
+// space: O(n)
