0112a738488b1e73c561fe3057a377f79c07f7b0
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-02-17 19:53:32 +0700

    Time: 3 ms (32.37%), Space: 4 MB (33.7%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.go
+++ 0190-reverse-bits/0190-reverse-bits.go
@@ -1,27 +1,25 @@
 func reverseBits(n int) int {
-    bits := make([]int, 32)
+	bits := make([]int, 32)
+	i := 0
+	for n > 0 {
+		bits[i] = n % 2
+		n /= 2
+		i++
+	}
 
-    i := 0
-    for n > 0 {
-        bits[i] = n % 2
-        n /= 2
-        i++
-    }
+	ans := 0
+	mul := 1
+	for i := 0; i < 31; i++ {
+		mul *= 2
+	}
+	for i := 0; i < len(bits); i++ {
+		ans += bits[i] * mul
+		mul /= 2
+	}
 
-    ans := 0
-    for i := 0; i < 32; i++ {
-        ans += bits[i] * pow2(31-i)
-    }
-
-    return ans
+	return ans
 }
 
-func pow2(x int) int {
-    res := 1
-    for i := 0; i < x; i++ {
-        res *= 2
-    }
-    return res
-}
-
-// notice me senpai
+// bit manipulation
+// time: O(1)
+// space: O(1)
