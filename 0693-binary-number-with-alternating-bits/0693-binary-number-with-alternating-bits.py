bab55a32874a13ac0f80610ce06643ffdd533c36
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-02-18 23:52:17 +0700

    Time: 4 ms (2.75%), Space: 12.5 MB (15.38%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def hasAlternatingBits(self, n):
+        bits = bin(n)
+        return all(bits[i] != bits[i+1]
+                   for i in xrange(len(bits) - 1))
+
+# notice me senpai
+# editorial
+# bit manipulation
