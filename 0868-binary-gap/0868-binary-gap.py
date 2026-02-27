7ca779b978381d240b83ac6a2d95e59639c65de7
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-02-27 14:09:24 +0700

    Time: 0 ms (100%), Space: 19.3 MB (93.86%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -1,8 +1,21 @@
-class Solution(object):
-    def binaryGap(self, N):
-        A = [i for i in xrange(32) if (N >> i) & 1]
-        if len(A) < 2: return 0
-        return max(A[i+1] - A[i] for i in xrange(len(A) - 1))
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        pos = []
+        idx = 0
+        while n > 0:
+            if n & 1 == 1:
+                pos.append(idx)
+            idx += 1
+            n >>= 1
+        
+        if len(pos) == 1: return 0
 
-# editorial
-# notice me senpai
+        ans = 0
+        for i in range(1, len(pos)):
+            ans = max(ans, pos[i] - pos[i - 1])
+        
+        return ans
+
+# bit manipulation
+# time: O(log(n))
+# space: O(log(n))
