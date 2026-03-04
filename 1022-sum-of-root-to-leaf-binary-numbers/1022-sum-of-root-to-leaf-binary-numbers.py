0d0b7a3e37d0c3d07df1a7cca9f41164f442cc03
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-03-04 14:40:24 +0700

    Time: 4 ms (6.73%), Space: 19.4 MB (99.1%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -1,6 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def sumRootToLeaf(self, r: Optional[TreeNode]) -> int:
-        return (f:=lambda n,q=0:n and (f(n.left,q:=q*2+n.val)+f(n.right,q) or q) or 0)(r)
+        ans = 0
+        bin = []
 
-# notice me senpai
-# solution from solutions (MikPosp)
+        def dfs(node):
+            nonlocal ans
+
+            if node == None: return
+
+            bin.append(node.val)
+
+            if node.left == None and node.right == None:
+                ans += binToDec(bin)
+                bin.pop() # backtrack
+                return
+
+            dfs(node.left)
+            dfs(node.right)
+
+            bin.pop() # backtrack
+        
+        dfs(r)
+
+        return(ans)
+
+# binary tree, dfs, tree
+# time: O(n^2)
+# space: O(n)
+
+def binToDec(b):
+    res = 0
+    mul = 1
+    for i in range(len(b) - 1, -1, -1):
+        if b[i] == 1: res += mul
+        mul *= 2
+    return res
+        
