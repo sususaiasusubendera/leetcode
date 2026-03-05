51d13f65823d9b3a809bdefc8bb72c89abe501bb
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-03-05 21:54:51 +0700

    Time: 0 ms (100%), Space: 4.3 MB (100%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.go
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.go
@@ -1,27 +1,28 @@
 func minOperations(s string) int {
-    // odd: 0, even: 1
-    opsZeroOdd := 0
+    // odd: 1, even: 0
+    opsZeroEven := 0
     for i := 0; i < len(s); i++ {
         if i % 2 == 0 {
             if s[i] == '1' {
-                opsZeroOdd++
+                opsZeroEven++
             }
         } else {
             if s[i] == '0' {
-                opsZeroOdd++
+                opsZeroEven++
             }
         }
     }
 
-    opsZeroEven := 0
+    // odd: 0, even: 1
+    opsZeroOdd := 0
     for i := 0; i < len(s); i++ {
         if i % 2 == 0 {
             if s[i] == '0' {
-                opsZeroEven++
+                opsZeroOdd++
             }
         } else {
             if s[i] == '1' {
-                opsZeroEven++
+                opsZeroOdd++
             }
         }
     }
