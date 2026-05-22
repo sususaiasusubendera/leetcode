af48ca9345313820ce6fc9e3a366720b74edcadd
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-05-22 15:26:34 +0700

    Time: 0 ms (100%), Space: 4.3 MB (70.88%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -0,0 +1,13 @@
+func search(nums []int, target int) int {
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == target {
+            return i
+        }
+    }
+
+    return -1
+}
+
+// array
+// time: O(n)
+// space: O(1)
