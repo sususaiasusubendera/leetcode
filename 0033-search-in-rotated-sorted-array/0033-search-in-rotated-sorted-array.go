787b4e1e9a05c317797d4f1ca0ead62368fb146b
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-05-22 16:38:33 +0700

    Time: 0 ms (100%), Space: 4.4 MB (70.88%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -1,13 +1,37 @@
 func search(nums []int, target int) int {
-    for i := 0; i < len(nums); i++ {
-        if nums[i] == target {
-            return i
+	left, right := 0, len(nums)-1
+    for left <= right {
+        mid := left + ((right - left) / 2)
+        if nums[mid] == target {
+            return mid
+        } 
+        
+        if nums[left] > nums[right] { // left rotated
+            if nums[left] <= nums[mid] { // left side is sorted, but right side is not
+                if nums[left] <= target && target < nums[mid] {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else { // right side is sorted, but left side is not
+                if nums[mid] < target && target <= nums[right] {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+        } else { // normal asc order
+            if nums[mid] < target {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
         }
     }
 
     return -1
 }
 
-// array
-// time: O(n)
+// array, binary search
+// time: O(log(n))
 // space: O(1)
