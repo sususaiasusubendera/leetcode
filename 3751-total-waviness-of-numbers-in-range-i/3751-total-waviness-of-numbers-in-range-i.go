80bf1feed22cb3846b96e1276bbe245a2187ac7c
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-06-15 15:01:42 +0700

    Time: 22 ms (31.63%), Space: 7.2 MB (55.26%) - LeetHub

---
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.go
@@ -0,0 +1,25 @@
+func totalWaviness(num1 int, num2 int) int {
+    ans := 0
+    for i := num1; i <= num2; i++ {
+        ans += solve(i)
+    }
+
+    return ans
+}
+
+func solve(n int) int {
+    s := strconv.Itoa(n)
+
+    if len(s) < 3 {
+        return 0
+    }
+    
+    waviness := 0
+    for i := 1; i < len(s)-1; i++ {
+        if (s[i-1] < s[i] && s[i] > s[i+1]) || (s[i-1] > s[i] && s[i] < s[i+1]) {
+            waviness++
+        }
+    }
+
+    return waviness
+}
