ec6d8c4de32f375938391f92ef1b4eec5928cbf2
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-06-16 19:40:55 +0700

    Time: 0 ms (100%), Space: 6.7 MB (98.08%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.go
@@ -0,0 +1,16 @@
+func mapWordWeights(words []string, weights []int) string {
+    ans := []byte{}
+    for _, word := range words {
+        weight := 0
+        for i := 0; i < len(word); i++ {
+            weight += weights[word[i] - 'a']
+        }
+
+        ans = append(ans, byte('z' - (weight % 26)))
+    }
+
+    return string(ans)
+}
+
+// time: O(nk)
+// space: O(n)
