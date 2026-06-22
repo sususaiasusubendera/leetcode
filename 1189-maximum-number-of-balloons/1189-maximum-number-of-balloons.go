5f16be570f7e2810521258d341190116cb148a8a
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-06-22 18:53:50 +0700

    Time: 1 ms (27.66%), Space: 4.2 MB (10.64%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.go
@@ -0,0 +1,24 @@
+func maxNumberOfBalloons(text string) int {
+    freq := map[rune]int{}
+    for _, c := range text {
+        freq[c]++
+    }
+
+    b, a, l, o, n := freq['b'], freq['a'], freq['l'], freq['o'], freq['n']
+
+    count := 0
+    for b > 0 && a > 0 && l > 1 && o > 1 && n > 0 {
+        count++
+        b--
+        a--
+        l -= 2
+        o -= 2
+        n--
+    }
+
+    return count
+}
+
+// hash map, string
+// time: O(n)
+// space: 
