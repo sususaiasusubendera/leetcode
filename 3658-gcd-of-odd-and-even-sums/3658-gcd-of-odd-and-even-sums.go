b3ca994986f86bc4a7e52a82b18aacd853996ea2
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-07-15 13:56:10 +0700

    Time: 0 ms (100%), Space: 4 MB (43.75%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.go
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.go
@@ -1,7 +1,23 @@
 func gcdOfOddEvenSums(n int) int {
-    return n
+    sumOdd, sumEven := 0, 0
+    for i := 1; i <= 2*n; i++ {
+        if i % 2 == 0 {
+            sumEven += i
+            continue
+        }
+        sumOdd += i
+    }
+
+    return gcd(sumOdd, sumEven)
+}
+
+func gcd(a, b int) int {
+    for b != 0 {
+        a, b = b, a % b
+    }
+    return a
 }
 
 // math
-// time: O(1)
+// time: O(n)
 // space: O(1)
