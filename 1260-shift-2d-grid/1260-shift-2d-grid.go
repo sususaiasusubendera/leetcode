d51b4c65b30d76dcc2a98b6e76247df60b8e82d0
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-07-20 13:35:59 +0700

    Time: 0 ms (100%), Space: 8.6 MB (72.73%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.go
@@ -0,0 +1,28 @@
+func shiftGrid(grid [][]int, k int) [][]int {
+	r, c := len(grid), len(grid[0])
+    n := r * c
+	k = k % n
+
+    if k == 0 { return grid }
+
+    rev(grid, 0, n-1)
+    rev(grid, 0, k-1)
+    rev(grid, k, n-1)
+
+    return grid
+}
+
+func rev(grid [][]int, i, j int) {
+    c := len(grid[0])
+    for i < j {
+        grid[i/c][i%c], grid[j/c][j%c] = grid[j/c][j%c], grid[i/c][i%c]
+        i++
+        j--
+    }
+}
+
+// array, simulation
+// time: O(n)
+// space: O(1)
+
+// this approach is (prolly) the most optimal one
