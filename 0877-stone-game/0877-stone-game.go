32b77e432576b65bde8703c4da5b2f7089d260ff
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-08-04 12:56:46 +0700

    Time: 13 ms (15.15%), Space: 9 MB (18.18%) - LeetHub

--- 0877-stone-game/0877-stone-game.go
+++ 0877-stone-game/0877-stone-game.go
@@ -1,7 +1,36 @@
 func stoneGame(piles []int) bool {
-    return true
+    n := len(piles)
+
+    memo := make([][]int, n) // memo[l][r] stores the result of dp(l, r)
+    visited := make([][]bool, n)
+
+    for i := 0; i < n; i++ {
+        memo[i] = make([]int, n)
+        visited[i] = make([]bool, n)
+    }
+
+    var dp func(l, r int) int
+    dp = func(l, r int) int {
+        if l == r {
+            return piles[l]
+        }
+
+        if visited[l][r] {
+            return memo[l][r]
+        }
+
+        left := piles[l] - dp(l+1, r)
+        right := piles[r] - dp(l, r-1)
+
+        memo[l][r] = max(left, right)
+        visited[l][r] = true
+
+        return memo[l][r]
+    }
+
+    return dp(0, n-1) >= 0
 }
 
-// meme
-// time: O(1)
-// space: O(1)
+// dp (top-down) + memoization
+// time: O(n^2)
+// space: O(n^2)
