f79eecd19f4b96228b4e7d613c0b3855f2c1cd1e
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-08-19 11:20:36 +0700

    Time: 31 ms (18.42%), Space: 9.2 MB (36.84%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.go
@@ -0,0 +1,43 @@
+func maxNumberOfFamilies(n int, reservedSeats [][]int) int {
+    ans := 2 * n // max possible result if all seats aren't reserved
+
+    sort.Slice(reservedSeats, func(i, j int) bool {
+        return reservedSeats[i][0] < reservedSeats[j][0]
+    })
+
+    idx := 0
+    for idx < len(reservedSeats) {
+        currRow := reservedSeats[idx][0]
+        // left: s2, s3 reserved
+        // midLeft: s4, s5 reserved
+        // midRight: s6, s7 reserved
+        // right: s8, s9 reserved
+        left, midLeft, midRight, right := false, false, false, false
+        for idx < len(reservedSeats) && reservedSeats[idx][0] == currRow {
+            seat := reservedSeats[idx][1]
+            if seat == 2 || seat == 3 {
+                left = true
+            } else if seat == 4 || seat == 5 {
+                midLeft = true
+            } else if seat == 6 || seat == 7 {
+                midRight = true
+            } else if seat == 8 || seat == 9 {
+                right = true
+            }
+
+            idx++
+        }
+
+        if (left && midRight) || (midLeft && midRight) || (midLeft && right) {
+            ans -= 2
+        } else if left || midLeft || midRight || right {
+            ans -= 1
+        }
+    }
+
+    return ans
+}
+
+// array, greedy, hash map
+// time: O(nlog(n))
+// space: O(1)
