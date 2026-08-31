7b129fa13e16bcb4fab061e930d9338c2ec57271
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-08-31 23:32:28 +0700

    Time: 19 ms (65.68%), Space: 33.5 MB (63.61%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,13 +1,26 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-        minidx = nums.index(min(nums))
-        maxidx = nums.index(max(nums))
-        l = min(minidx, maxidx)  # The smaller value in the most valuable index
-        r = max(minidx, maxidx)  # The bigger value in the most valuable index
-        n = len(nums)
-        return min(
-            r + 1, n - l, l + 1 + n - r
-        )  # Calculate the minimum number of deletions in three cases
+        if len(nums) == 1:
+            return 1
 
-# notice me senpai (easy, but no time)
-# editorial
+        n = len(nums)
+        
+        minIdx = maxIdx = -1
+        minVal, maxVal = 100000, -100000
+        for i in range(n):
+            if nums[i] < minVal:
+                minVal = nums[i]
+                minIdx = i
+            
+            if nums[i] > maxVal:
+                maxVal = nums[i]
+                maxIdx = i
+            
+        left = min(minIdx, maxIdx)
+        right = max(minIdx, maxIdx)
+
+        return min(right + 1, n - left, left + 1 + n - right)
+
+# array, greedy
+# time: O(n)
+# space: O(1)
