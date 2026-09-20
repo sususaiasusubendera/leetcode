f00c4e43db2ce6059c5cca41a7f2d43a1567905c
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2026-09-20 15:52:04 +0700

    Time: 14 ms (72.79%), Space: 19.1 MB (99.28%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -1,24 +1,23 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        n = len(digits)
-        vis = [False] * 1000
+        # 3-digit even numbers
+        seen = [False] * 1000
         ans = 0
-
-        for i in range(n):
+        for i in range(len(digits)):
             if digits[i] == 0:
                 continue
-            for j in range(n):
+            for j in range(len(digits)):
                 if j == i:
                     continue
-                for k in range(n):
+                for k in range(len(digits)):
                     if k == i or k == j or digits[k] % 2 != 0:
                         continue
-                    x = digits[i] * 100 + digits[j] * 10 + digits[k]
-                    if not vis[x]:
-                        vis[x] = True
+                    num = digits[i] * 100 + digits[j] * 10 + digits[k]
+                    if not seen[num]:
+                        seen[num] = True
                         ans += 1
-
         return ans
 
-# notice me senpai
-# editorial
+# array, brute force
+# time: O(n^3)
+# space: O(1)
